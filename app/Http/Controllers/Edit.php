2318e68667de4da72127427ad64d5bf05b1f90c9
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-26 10:44:45 +0000

    bug fixed route

--- app/Http/Controllers/Edit.php
+++ app/Http/Controllers/Edit.php
@@ -7,19 +7,20 @@ use Illuminate\Support\Facades\DB;
 
 class Edit extends Controller
 {
-    public function edit(Request $request){
-        $id=$request->id;
-        $new_company_name=$request->new_company_name;
-        $new_title=$request->new_title;
-        $new_comment=$request->new_comment;
+    public function edit(Request $request)
+    {
+        $id = $request->id;
+        $new_company_name = $request->new_company_name;
+        $new_title = $request->new_title;
+        $new_comment = $request->new_comment;
 
         DB::table('comments')
-            ->where('id','=',$id)
+            ->where('id', '=', $id)
             ->update([
-                'company_name'=>$new_company_name,
-                'commenttitle'=>$new_title,
-                'comment'=>$new_comment
+                'company_name' => $new_company_name,
+                'commenttitle' => $new_title,
+                'comment' => $new_comment
             ]);
-        return response()->json(['message'=>'success']);
+        return response()->json(['message' => 'success']);
     }
 }
