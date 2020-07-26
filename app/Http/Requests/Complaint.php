d88fe4a41a7d14f7e01398937d9176cde9c5809d
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-26 10:35:04 +0000

    Added auth check to Complaint request

--- app/Http/Requests/Complaint.php
+++ app/Http/Requests/Complaint.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class Complaint extends FormRequest
 {
@@ -13,7 +14,12 @@ class Complaint extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        if(Auth::user()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -24,30 +30,31 @@ class Complaint extends FormRequest
     public function rules()
     {
         return [
-           'email'=>'required|email|min:8|max:70',
-            'company_name'=>'required|string|min:5|max:70',
-            'complaint_title'=>'required|string|min:30|max:300',
-            'complaint_body'=>'required|string|min:100|max:10000',
+            'email' => ['required', 'email', 'min:8', 'max:70'],
+            'company_name' => 'required|string|min:5|max:70',
+            'complaint_title' => 'required|string|min:30|max:300',
+            'complaint_body' => 'required|string|min:100|max:10000',
         ];
     }
+
     public function messages()
     {
-       return[
-            'email.required'=>'Zəhmət olmasa email adresinizi girin',
-            'email.max'=>'Daxil etdiyiniz email  uzundur',
-            'email.min'=>'Daxil etdiyiniz email  qısadır',
+        return [
+            'email.required' => 'Zəhmət olmasa email adresinizi girin',
+            'email.max' => 'Daxil etdiyiniz email  uzundur',
+            'email.min' => 'Daxil etdiyiniz email  qısadır',
 
-           'company_name.required'=>'Zəhmət olmasa şikayətçi olduğunuz şirkət adını daxil edin ',
-           'company_name.max'=>'Daxil etdiyiniz şirkət adı  uzundur',
-           'company_name.min'=>'Daxil etdiyiniz şirkət adı   qısadır',
+            'company_name.required' => 'Zəhmət olmasa şikayətçi olduğunuz şirkət adını daxil edin ',
+            'company_name.max' => 'Daxil etdiyiniz şirkət adı  uzundur',
+            'company_name.min' => 'Daxil etdiyiniz şirkət adı   qısadır',
 
-           'complaint_title.min'=>'Daxil etdiyiniz başlıq məlumatı  qısadır',
-           'complaint_title.max'=>'Daxil etdiyiniz başlıq məlumatı  uzundur',
-           'complaint_title.required'=>'Zəhmət olmasa başlıq qeyd edin',
+            'complaint_title.min' => 'Daxil etdiyiniz başlıq məlumatı  qısadır',
+            'complaint_title.max' => 'Daxil etdiyiniz başlıq məlumatı  uzundur',
+            'complaint_title.required' => 'Zəhmət olmasa başlıq qeyd edin',
 
-           'complaint_body.required'=>'Zəhmət olmasa şikayətinizi daxil edin',
-           'complaint_body.min'=>'Daxil etdiyiniz məlumat  qısadır',
-           'complaint_body.max'=>'Daxil etdiyiniz məlumat  uzundur',
-       ];
+            'complaint_body.required' => 'Zəhmət olmasa şikayətinizi daxil edin',
+            'complaint_body.min' => 'Daxil etdiyiniz məlumat  qısadır',
+            'complaint_body.max' => 'Daxil etdiyiniz məlumat  uzundur',
+        ];
     }
 }
