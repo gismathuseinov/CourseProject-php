ed5334f732ca9d0ac51b2450af02d3f8dcb56cb9
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-26 14:21:41 +0000

    Route Update

--- routes/web.php
+++ routes/web.php
@@ -21,21 +21,20 @@ Route::post('/search', 'IndexController@search');
 Route::get('/sikayet', 'IndexController@sikeytet');
 Route::get('/logout', 'Signout@exit')->name('logout');
 Route::get('/post/view/{id}', 'IndexController@post')->name("post.view");
+Route::post('/send', 'ContactController@send');
+
 
 Route::middleware(['auth'])->group(function () {
     Route::post('/create_post', 'ComplaintController@create_post');
     Route::post('/post_comments', 'IndexController@post_comment');
-    Route::post('/send', 'ContactController@send');
     Route::post('/check', 'Check@check');
 });
 //admin
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
     Route::get('/admin/issue', 'AdminController@issue')->name('admin.issue');
-    Route::get('/admin/see/{id}', 'AdminController@see')->name('admin.see');
     Route::get('/admin/message', 'AdminController@message')->name('admin.message');
-    Route::get('/admin/error', 'AdminController@error');
-    Route::get('/admin/start', 'AdminController@start');
+    Route::post('/see', 'AdminController@see');
     Route::post('/delete', 'Delete@delete');
     Route::post('/edit', 'Edit@edit');
     Route::post('/accept', 'Accept@accept');
