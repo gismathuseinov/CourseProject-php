c2b11a5824482d83421a6b02caa553d86c763d8f
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-26 10:44:03 +0000

    Fixed delete function on Delete Model

--- app/Http/Controllers/Delete.php
+++ app/Http/Controllers/Delete.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Comments;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class Delete extends Controller
 {
-   public  function delete(Request $request){
-       $id=$request->id;
-      $delete =   DB::table('comments')
-            ->where('is_letted','=','0')->where('id',$id)
-            ->delete();
-   }
+    public function delete(Request $request)
+    {
+        $id = $request->id;
+
+        Comments::where('is_letted', 0)->where('id', $id)->delete();
+    }
 }
