8ee30202be0f0de2d27ba7abb6f8ff8411ff127e
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-27 14:44:07 +0000

    Fixed AdminPanel

--- resources/views/proyekt/nice-html/ltr/index.blade.php
+++ resources/views/proyekt/nice-html/ltr/index.blade.php
@@ -76,70 +76,83 @@
                                                 <strong>All Users</strong>
                                             </div>
                                             <div class="number dashtext-1">
+                                                {{$userCount}}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style="float: left" class="col-md-3 col-sm-6">
+                                    <div class="statistic-block block">
+                                        <div class="progress-details d-flex align-items-end justify-content-between">
+                                            <div class="title">
+                                                <div class="icon"><i class="icon-user-1"></i></div>
+                                                <strong>Unread Messages</strong>
+                                            </div>
+                                            <div class="number dashtext-1">
+                                                {{$unReadMessage}}
+                                            </div>
+                                        </div>
+                                        <div class="progres">
+                                            <div>
+                                                <button class="btn btn-outline-warning"><a href="{{route('admin.message')}}">Show</a></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style="float: left" class="col-md-3 col-sm-6">
+                                    <div class="statistic-block block">
+                                        <div class="progress-details d-flex align-items-end justify-content-between">
+                                            <div class="title">
+                                                <div class="icon"><i class="icon-user-1"></i></div>
+                                                <strong>Receive Wait Complaint</strong>
+                                            </div>
+                                            <div class="number dashtext-1">
+                                                {{$newComplaint}}
+                                            </div>
+                                        </div>
+                                        <div class="progres">
+                                            <div>
+                                                <button class="btn btn-outline-warning"><a href="{{route('admin.issue')}}">Show</a></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-                                            </div>
-                                        </div>
-                                        <div class="progres">
-                                            <div>
-                                                <button class="btn btn-outline-warning">Show</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div style="float: left" class="col-md-3 col-sm-6">
-                                    <div class="statistic-block block">
-                                        <div class="progress-details d-flex align-items-end justify-content-between">
-                                            <div class="title">
-                                                <div class="icon"><i class="icon-user-1"></i></div>
-                                                <strong>Resend Messages</strong>
-                                            </div>
-                                            <div class="number dashtext-1">
-                                                60
-                                            </div>
-                                        </div>
-                                        <div class="progres">
-                                            <div>
-                                                <button class="btn btn-outline-warning">Show</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div style="float: left" class="col-md-3 col-sm-6">
-                                    <div class="statistic-block block">
-                                        <div class="progress-details d-flex align-items-end justify-content-between">
-                                            <div class="title">
-                                                <div class="icon"><i class="icon-user-1"></i></div>
-                                                <strong>New Complaint</strong>
-                                            </div>
-                                            <div class="number dashtext-1">
-                                                60
-                                            </div>
-                                        </div>
-                                        <div class="progres">
-                                            <div>
-                                                <button class="btn btn-outline-warning">Show</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div style="float: left" class="col-md-3 col-sm-6">
-                                    <div class="statistic-block block">
-                                        <div class="progress-details d-flex align-items-end justify-content-between">
-                                            <div class="title">
-                                                <div class="icon"><i class="icon-user-1"></i></div>
-                                                <strong>Wait receive complaint</strong>
-                                            </div>
-                                            <div class="number dashtext-1">
-                                                60
-                                            </div>
-                                        </div>
-                                        <div class="progres">
-                                            <div>
-                                                <button class="btn btn-outline-warning">Show</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive" id="users">
+                                <h1>Users</h1>
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Last Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Number</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($users as $key => $user)
+                                        <tr>
+                                            <th scope="row">{{$user->id}}</th>
+                                            <td>{{$user->name}}</td>
+                                            <td>{{$user->surname}}</td>
+                                            <td>{{$user->email}}</td>
+                                            <td>{{$user->number}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                            </div>
+                            <div class="pagination">
+                              {{$users->links()}}
                             </div>
                         </div>
                     </div>
