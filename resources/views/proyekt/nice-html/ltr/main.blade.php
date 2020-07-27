abd73fcdfb056d0c220e6956309dc181720f8627
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-27 14:44:27 +0000

    Initial commit

--- resources/views/proyekt/nice-html/ltr/main.blade.php
+++ resources/views/proyekt/nice-html/ltr/main.blade.php
@@ -4,16 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('Project/assets/images/favicon.png')}}">
     <title>Nice admin Template - The Ultimate Multipurpose admin template</title>
-    <!-- Custom CSS -->
     <link href="{{asset('Project/assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
-    <!-- Custom CSS -->
     <link href="{{asset('Project/dist/css/style.min.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -29,7 +25,7 @@
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
 </head>
 
@@ -37,66 +33,36 @@
 
 <div id="main-wrapper" data-navbarbg="skin6" data-theme="light" data-layout="vertical" data-sidebartype="full"
      data-boxed-layout="full">
-    <!-- ============================================================== -->
-    <!-- Topbar header - style you can find in pages.scss -->
-    <!-- ============================================================== -->
     <header class="topbar" data-navbarbg="skin6">
         <nav class="navbar top-navbar navbar-expand-md navbar-light">
             <div class="navbar-header" data-logobg="skin5">
-                <!-- This is for the sidebar toggle which is visible on mobile only -->
                 <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)">
                     <i class="ti-menu ti-close"></i>
                 </a>
-                <!-- ============================================================== -->
-                <!-- Logo -->
-                <!-- ============================================================== -->
                 <div class="navbar-brand">
                     <a href="{{route('admin.dashboard')}}" class="logo">
-                        <!-- Logo icon -->
                         <b class="logo-icon">
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
                             <img src="{{asset('Project/assets/images/logo-icon.png')}}" alt="homepage"
                                  class="dark-logo"/>
-                            <!-- Light Logo icon -->
                             <img src="{{asset('Project/assets/images/logo-light-icon.png')}}" alt="homepage"
                                  class="light-logo"/>
                         </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text -->
                         <span class="logo-text">
-                                <!-- dark Logo text -->
                                 <img src="{{asset('Project/assets/images/logo-text.png')}}" alt="homepage"
                                      class="dark-logo"/>
-                            <!-- Light Logo text -->
                                 <img src="{{asset('Project/assets/images/logo-light-text.png')}}" class="light-logo"
                                      alt="homepage"/>
                             </span>
                     </a>
                 </div>
-                <!-- ============================================================== -->
-                <!-- End Logo -->
-                <!-- ============================================================== -->
-                <!-- ============================================================== -->
-                <!-- Toggle which is visible on mobile only -->
-                <!-- ============================================================== -->
                 <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)"
                    data-toggle="collapse" data-target="#navbarSupportedContent"
                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="ti-more"></i>
                 </a>
             </div>
-            <!-- ============================================================== -->
-            <!-- End Logo -->
-            <!-- ============================================================== -->
             <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin6">
-                <!-- ============================================================== -->
-                <!-- toggle and nav items -->
-                <!-- ============================================================== -->
                 <ul class="navbar-nav float-left mr-auto">
-                    <!-- ============================================================== -->
-                    <!-- Search -->
-                    <!-- ============================================================== -->
                     <li class="nav-item search-box">
                         <a class="nav-link waves-effect waves-dark" href="javascript:void(0)">
                             <div class="d-flex align-items-center">
@@ -114,10 +80,6 @@
                         </form>
                     </li>
                 </ul>
-                <!-- ============================================================== -->
-                <!-- Right side toggle and nav items -->
-                <!-- ============================================================== -->
-
             </div>
         </nav>
     </header>
@@ -157,31 +119,15 @@
                     </li>
                 </ul>
             </nav>
-            <!-- End Sidebar navigation -->
         </div>
-        <!-- End Sidebar scroll-->
     </aside>
 
     @yield('main')
 
-    <footer class="footer text-center">
-        All Rights Reserved by Nice admin. Designed and Developed by
-        <b>CoDeT</b>.
-    </footer>
-    <!-- ============================================================== -->
-    <!-- End footer -->
-    <!-- ============================================================== -->
+
 </div>
-<!-- ============================================================== -->
-<!-- End Page wrapper  -->
-<!-- ============================================================== -->
 </div>
-<!-- ============================================================== -->
-<!-- End Wrapper -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- All Jquery -->
-<!-- ============================================================== -->
+
 <script src="{{asset('Project/assets/libs/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap tether Core JavaScript -->
 <script src="{{asset('Project/assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
