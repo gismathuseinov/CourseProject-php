7c4a3cc02ef89dc73a8a87d78ea699642f4ba498
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-28 13:53:50 +0000

    Fixed Relation on PostComment

--- app/PostComment.php
+++ app/PostComment.php
@@ -12,4 +12,9 @@ class PostComment extends Model
     {
         return $this->hasOne(Complaint::class);
     }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
