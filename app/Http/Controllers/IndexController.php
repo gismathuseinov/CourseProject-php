be59838370ea9ad984065c264c9dd3284e64c852
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-30 00:13:45 +0000

    Delete and Move some functions on IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Complaint;
-use App\PostComment;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
-
     public function index(Request $request)
     {
         $userCount = User::count();
@@ -21,7 +18,6 @@ class IndexController extends Controller
 
         return view('proyekt.index', compact(['userCount', 'issueCount', 'comments']));
     }
-
     public function about()
     {
         return view('proyekt.about');
@@ -31,53 +27,9 @@ class IndexController extends Controller
     {
         return view('proyekt.goindex');
     }
-
-    public function contact()
-    {
-        return view('proyekt.contactus');
-    }
-
-    public function complaint()
-    {
-        $comment = Complaint::all();
-        return view('proyekt.complaint', compact('comment'));
-    }
-
     public function sikeytet()
     {
         return view('proyekt.sikayetet');
-    }
-
-    public function post(Request $request, int $id)
-    {
-        $post = Complaint::findOrFail($id);
-
-        $comments = PostComment::where('complaint_id', $id)->orderBy('id', 'desc')->get();
-
-        return view('proyekt.post', compact(['post', 'comments']));
-    }
-
-    public function comment_create(\App\Http\Requests\PostComment $request, int $post_id)
-    {
-        $user = \auth()->user();
-
-        $name = $user->name;
-
-        $post = Complaint::findOrFail($post_id);
-
-        $validated = $request->validated();
-
-        $comment = $post->comments()->create([
-            'user_id' => $user->id,
-            'comments' => $validated['comments'],
-        ]);
-
-        return response()->json([
-            'status' => 'success',
-            'name' => $name,
-            'comment' => $comment->comments,
-            'time' => date("H:i", strtotime($comment->created_at))
-        ]);
     }
 
     public function get_post_comments(int $post_id)
@@ -89,21 +41,5 @@ class IndexController extends Controller
             'comments' => $post->comments,
         ]);
     }
-
-    public function search(Request $request)
-    {
-        $data = $request->data;
-        $result = DB::table('comments')
-            ->leftJoin('users', 'comments.user_id', '=', 'users.id')
-            ->select('comments.commenttitle', 'users.name', 'comments.comment', 'comments.id', 'comments.company_name', 'comments.created_at')
-            ->where('comments.comment', 'LIKE', "%{$data}%")
-            ->orWhere('comments.company_name', 'LIKE', "%{$data}%")
-            ->orWhere('comments.commenttitle', 'LIKE', "%{$data}%")
-            ->get();
-        return response()->json(['message' => 'success', 'result' => $result]);
-
-    }
-
-
 }
 
