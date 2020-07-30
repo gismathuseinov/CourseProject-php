1b66622c6e574bd2648a29eb783fb95bb46771dd
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-30 00:13:18 +0000

    Add function ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Feedback;
-use Illuminate\Http\Request;
 
 
 class ContactController extends Controller
 {
+    public function contact()
+    {
+        return view('proyekt.contactus');
+    }
+
     public function send(Feedback $request)
     {
         \App\Feedback::create($request->validated());
 
         return response()->json([
             'status' => true,
-            'message' => 'Bizimlə əlaqə saxladığınız üçün təşəkkürlər'
+            'message' => 'Bizimlə əlaqə saxladığınız üçün təşəkkürlər!'
         ]);
     }
 }
