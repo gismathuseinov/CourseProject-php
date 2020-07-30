3d9d86f9d8a62325705a36e5d7378ed1ebe07571
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-30 00:09:02 +0000

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'IndexController@main');
 Route::get('/index', 'IndexController@index')->name('site.index');
 Route::get('/about', 'IndexController@about')->name('about');
-Route::get('/contact', 'IndexController@contact')->name('contact');
-Route::get('/complaint', 'IndexController@complaint')->name('complaint');
-Route::post('/search', 'IndexController@search');
-Route::get('/sikayet', 'IndexController@sikeytet');
+Route::get('/contact', 'ContactController@contact')->name('contact');
+Route::get('/complaint', 'ComplaintController@complaint')->name('complaint');
+Route::post('/search', 'SearchController@search')->name('search');
+Route::get('/sikayet', 'IndexController@sikeytet')->name('write.complaint');
 Route::get('/logout', 'Signout@exit')->name('logout');
-Route::get('/post/view/{id}', 'IndexController@post')->name("post.view");
+Route::get('/post/view/{id}', 'ComplaintController@post')->name("post.view");
 Route::post('/send', 'ContactController@send');
 
 Route::get('/post/{post_id}/comments', 'IndexController@get_post_comments')->name('post.get.comments');
 
 Route::middleware(['auth'])->group(function () {
     Route::post('/create_post', 'ComplaintController@create_post');
-    Route::post('/post/{post_id}/comment/create', 'IndexController@comment_create')->name('post.create.comment');
-
-//    Route::post('/post/{post_id}/comment/{comment_id}/edit', 'IndexController@post_comment');
-
-    Route::post('/check', 'Check@check');
+    Route::post('/post/{post_id}/comment/create', 'CommentController@comment_create')->name('post.create.comment');
+    Route::post('/check', 'CheckController@check');
 });
 //admin
 Route::middleware(['auth', 'admin'])->group(function () {
@@ -39,9 +36,9 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/issue', 'AdminController@issue')->name('admin.issue');
     Route::get('/admin/message', 'AdminController@message')->name('admin.message');
     Route::post('/see', 'AdminController@see');
-    Route::post('/delete', 'Delete@delete');
-    Route::post('/edit', 'Edit@edit');
-    Route::post('/accept', 'Accept@accept');
+    Route::post('/delete', 'DeleteController@delete');
+    Route::post('/edit', 'EditController@edit');
+    Route::post('/accept', 'AcceptController@accept');
 });
 Auth::routes();
 
