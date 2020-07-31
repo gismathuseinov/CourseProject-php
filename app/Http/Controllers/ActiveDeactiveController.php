e4e238f53a468cd6e6f1443f96e894e2fe0dc177
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-07-31 21:53:47 +0000

    Fixed Post active/deactive on ActiveDeactiveController

--- app/Http/Controllers/ActiveDeactiveController.php
+++ app/Http/Controllers/ActiveDeactiveController.php
@@ -7,15 +7,32 @@ use Illuminate\Http\Request;
 
 class ActiveDeactiveController extends Controller
 {
-    public function active_deactive(Request $request, int $post_id)
+    public function active_deactive(Request $request)
     {
+        $post_id = $request->post_id;
+        $is_active = $request->is_active;
+        if ($is_active==1){
+            Complaint::where('id',$post_id)->update([
+                'is_active' => 1
+            ]);
+            return response()->json([
+                'status'=>true,
+                'id' => $post_id,
+                'message' => 'Post Aktivdir'
+            ]);
+        }
+        else{
+            Complaint::where('id',$post_id)->update([
+                'is_active' => 0
+            ]);
+            return response()->json([
+                'status'=>true,
+                'id' => $post_id,
+                'message' => 'Post Deaktivdir'
+            ]);
+        }
 
-       $post = Complaint::findOrFail($post_id);
-       $post->update($request->all());
-        return response()->json([
-            'status' => 'ok',
-            'message' => 'Good'
-        ]);
+
     }
 
 }
