b39cb8ea9eabbb7c10d119c38eca3eaddfa94687
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-08-22 12:36:22 +0400

    Update

--- resources/views/proyekt/sikayetet.blade.php
+++ resources/views/proyekt/sikayetet.blade.php
@@ -23,22 +23,13 @@
                     <textarea style="resize: none;" name="complaint_body" id="" cols="60" rows="5"
                               placeholder="Şikayətinizi detallı şəkildə ifadə edin"></textarea>
                 </div>
-                <div class="form-group">
-                    <label for="compphoto">Şikayətiniz firmaya/şirkətə göndərilsin?</label>
-                    <br>
-                    <label for="">Bəli</label>
-                    <input type="radio" name="is_send" value="1">
-                    <br>
-                    <label for="">Xeyr</label>
-                    <input type="radio" name="is_send" value="0">
-                </div>
             </div>
         </form>
         <div class="form-group buton">
-            <button class="btn btn-success">Gonder</button>
+            <button style="margin-top:160px;" class="btn btn-success">Göndər</button>
         </div>
     </div>
-    <br><br><br><br>
+    <br>
     <script>
         $('.btn-success').click(function () {
 
@@ -46,7 +37,6 @@
             var email = $('input[name=email]').val();
             var complaint_title = $('input[name=complaint_title]').val();
             var complaint_body = $('textarea[name=complaint_body]').val();
-            var is_send = $('input[name=is_send]').val();
             $.ajax({
                 type: "POST",
                 url: '/create_post',
@@ -55,7 +45,6 @@
                     'email': email,
                     'complaint_title': complaint_title,
                     'complaint_body': complaint_body,
-                    'is_send': is_send,
                     "_token": "{{ csrf_token() }}",
                 },
                 success: function (response) {
@@ -64,7 +53,6 @@
                     $('input[name=email]').val("");
                     $('input[name=complaint_title]').val("");
                     $('textarea[name=complaint_body]').val("");
-                    $('input[name=is_send]').removeAttr("checked");
                 },
                 error: function (request, error, response) {
                     toastr.error(request.responseJSON.errors[Object.keys(request.responseJSON.errors)[0]]);
